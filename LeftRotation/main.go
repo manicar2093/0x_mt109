803d15277d9a948237117e69818465db4d5c0eba
Manuel Carbajal <manicar2093@gmail.com>
2021-03-06 22:10:29 -0600

    Se agrega ejemplo del uso de la implemetación de la LinkedList

--- LeftRotation/main.go
+++ LeftRotation/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/manicar2093/leftRotation/lista"
 )
 
 func main() {
-	fmt.Println("Hola!")
+	l := lista.LinkedList{}
+
+	l.Insert(1)
+	l.Insert(2)
+	l.Insert(3)
+	l.Insert(4)
+	l.Insert(5)
+
+	fmt.Println(l.Display())
+
+	l.Delete()
+	fmt.Println(l.Display())
+
+	l.Shift(2)
+	fmt.Println(l.Display())
 
 }
